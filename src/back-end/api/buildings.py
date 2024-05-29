eaad9c56d590179a633153f3e396c246303892ef
heidisong0814 <heidisong0814@gmail.com>
2024-05-29 15:41:40 -0700

    slight changes in buildings and adding ccvi

--- src/back-end/api/buildings.py
+++ src/back-end/api/buildings.py
@@ -12,7 +12,53 @@ import json
 
 client = Socrata("data.cityofchicago.org", None)
 
-results = client.get("ydr8-5enu", limit=10)
+query = """SELECT id \
+    ,permit_ \
+    ,permit_status \
+    ,permit_milestone \
+    ,permit_type \
+    ,review_type \
+    ,application_start_date \
+    ,issue_date \
+    ,processing_time \
+    ,street_number \
+    ,street_direction \
+    ,street_name \
+    ,work_type \
+    ,work_description \
+    ,building_fee_paid \
+    ,zoning_fee_paid \
+    ,other_fee_paid \
+    ,subtotal_paid \
+    ,building_fee_unpaid \
+    ,zoning_fee_unpaid \
+    ,other_fee_unpaid \
+    ,subtotal_unpaid \
+    ,building_fee_waived \
+    ,building_fee_subtotal \
+    ,zoning_fee_subtotal \
+    ,other_fee_subtotal \
+    ,zoning_fee_waived \
+    ,other_fee_waived \
+    ,subtotal_waived \
+    ,total_fee \
+    ,contact_1_type \
+    ,contact_1_name \
+    ,contact_1_city \
+    ,contact_1_state \
+    ,contact_1_zipcode \
+    ,reported_cost \
+    ,pin1 \
+    ,community_area \
+    ,census_tract \
+    ,ward \
+    ,xcoordinate \
+    ,ycoordinate \
+    ,latitude \
+    ,longitude \
+    limit 20000 """
+
+results = client.get("ydr8-5enu", query=query)
 
 results_df = pd.DataFrame.from_records(results)
 
@@ -74,96 +120,15 @@ CREATE TABLE IF NOT EXISTS buildings (
     contact_1_city TEXT,
     contact_1_state TEXT,
     contact_1_zipcode TEXT,
-    contact_2_type TEXT,
-    contact_2_name TEXT,
-    contact_2_city TEXT,
-    contact_2_state TEXT,
-    contact_2_zipcode TEXT,
-    contact_3_type TEXT,
-    contact_3_name TEXT,
-    contact_3_city TEXT,
-    contact_3_state TEXT,
-    contact_3_zipcode TEXT,
-    contact_4_type TEXT,
-    contact_4_name TEXT,
-    contact_4_city TEXT,
-    contact_4_state TEXT,
-    contact_4_zipcode TEXT,
-    contact_5_type TEXT,
-    contact_5_name TEXT,
-    contact_5_city TEXT,
-    contact_5_state TEXT,
-    contact_5_zipcode TEXT,
-    contact_6_type TEXT,
-    contact_6_name TEXT,
-    contact_6_city TEXT,
-    contact_6_state TEXT,
-    contact_6_zipcode TEXT,
-    contact_7_type TEXT,
-    contact_7_name TEXT,
-    contact_7_city TEXT,
-    contact_7_state TEXT,
-    contact_7_zipcode TEXT,
-    contact_8_type TEXT,
-    contact_8_name TEXT,
-    contact_8_city TEXT,
-    contact_8_state TEXT,
-    contact_8_zipcode TEXT,
-    contact_9_type TEXT,
-    contact_9_name TEXT,
-    contact_9_city TEXT,
-    contact_9_state TEXT,
-    contact_9_zipcode TEXT,
-    contact_10_type TEXT,
-    contact_10_name TEXT,
-    contact_10_city TEXT,
-    contact_10_state TEXT,
-    contact_10_zipcode TEXT,
-    contact_11_type TEXT,
-    contact_11_name TEXT,
-    contact_11_city TEXT,
-    contact_11_state TEXT,
-    contact_11_zipcode TEXT,
-    contact_12_type TEXT,
-    contact_12_name TEXT,
-    contact_12_city TEXT,
-    contact_12_state TEXT,
-    contact_12_zipcode TEXT,
-    contact_13_type TEXT,
-    contact_13_name TEXT,
-    contact_13_city TEXT,
-    contact_13_state TEXT,
-    contact_13_zipcode TEXT,
-    contact_14_type TEXT,
-    contact_14_name TEXT,
-    contact_14_city TEXT,
-    contact_14_state TEXT,
-    contact_14_zipcode TEXT,
-    contact_15_type TEXT,
-    contact_15_name TEXT,
-    contact_15_city TEXT,
-    contact_15_state TEXT,
-    contact_15_zipcode TEXT,
     reported_cost NUMERIC,
-    pin_list TEXT,
     pin1 TEXT,
-    pin2 TEXT,
-    pin3 TEXT,
-    pin4 TEXT,
-    pin5 TEXT,
-    pin6 TEXT,
-    pin7 TEXT,
-    pin8 TEXT,
-    pin9 TEXT,
-    pin10 TEXT,
     community_area NUMERIC,
     census_tract NUMERIC,
     ward NUMERIC,
     xcoordinate NUMERIC,
     ycoordinate NUMERIC,
     latitude NUMERIC,
-    longitude NUMERIC,
-    location POINT
+    longitude NUMERIC
 )
 ''')
 conn.commit()
@@ -171,3 +136,5 @@ conn.commit()
 engine = create_engine('postgresql+psycopg2://hs:password1@localhost:5432/chicago')
 
 load_data_to_postgresql(results_df)
+
+conn.close()
---
+++ src/back-end/api/ccvi.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+
+# make sure to install these packages before running:
+# pip install pandas
+# pip install sodapy
+
+import pandas as pd
+from sodapy import Socrata
+from sqlalchemy import create_engine
+import psycopg2
+import json
+
+client = Socrata("data.cityofchicago.org", None)
+
+query = """SELECT geography_type \
+    ,community_area_or_zip \
+    ,community_area_name \
+    ,ccvi_score \
+    ,ccvi_category  \
+    ,rank_socioeconomic_status \
+    ,rank_household_composition \
+    ,rank_adults_no_pcp \
+    ,rank_cumulative_mobility_ratio\
+    ,rank_frontline_essential_workers \
+    ,rank_age_65_plus \
+    ,rank_comorbid_conditions \
+    ,rank_covid_19_incidence_rate \
+    ,rank_covid_19_hospital_admission_rate \
+    ,rank_covid_19_crude_mortality_rate \
+    ,location \
+    limit 20000 """
+
+results = client.get("xhc6-88s9", query=query)
+
+results_df = pd.DataFrame.from_records(results)
+
+def convert_complex_types(df):
+    for col in df.columns:
+        if df[col].apply(lambda x: isinstance(x, (dict, list))).any():
+            df[col] = df[col].apply(lambda x: json.dumps(x) if isinstance(x, (dict, list)) else x)
+    return df
+
+results_df = convert_complex_types(results_df)
+
+def load_data_to_postgresql(df):
+    engine = create_engine('postgresql+psycopg2://hs:password1@localhost/postgres')
+    df.to_sql('ccvi', con=engine, if_exists='replace', index=False)
+
+conn = psycopg2.connect(
+    host="localhost",  
+    database="postgres",  
+    user="hs", 
+    password="password1" 
+)
+
+cursor = conn.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS ccvi (
+    geography_type TEXT,
+    community_area_or_zip NUMERIC,
+    community_area_name TEXT,
+    ccvi_score NUMERIC,
+    ccvi_category TEXT,
+    rank_socioeconomic_status NUMERIC,
+    rank_household_composition NUMERIC,
+    rank_adults_no_pcp NUMERIC,
+    rank_cumulative_mobility_ratio NUMERIC,
+    rank_frontline_essential_workers NUMERIC,
+    rank_age_65_plus NUMERIC,
+    rank_comorbid_conditions NUMERIC,
+    rank_covid_19_incidence_rate NUMERIC, 
+    rank_covid_19_hospital_admission_rate NUMERIC,
+    rank_covid_19_crude_mortality_rate NUMERIC,
+    location TEXT
+)
+''')
+conn.commit()
+
+engine = create_engine('postgresql+psycopg2://hs:password1@localhost:5432/chicago')
+
+load_data_to_postgresql(results_df)
+
+conn.close()
